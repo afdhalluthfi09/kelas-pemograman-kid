9c9e70f995134cfe89f82527e9547fe0ffb3d002
Afdhal Luthfi <39642502+afdhalluthfi09@users.noreply.github.com>
2023-01-04 01:51:59 +0000

    day-2

---
+++ Javascript/day-1/script.js
@@ -0,0 +1,25 @@
+console.log('halo saya javascript')
+var nama ='Luthfi'
+console.log('nama saya'+nama)
+nama ='afdhal'
+console.log('nama saya'+nama);
+var lokasi ='bantul';
+lokasi ='sleman';
+console.log(lokasi);
+
+///// belajar type variable let
+let siswa ='nama';
+if(siswa == 'nama'){
+    let usia =30
+    console.log(usia);
+}
+/// biodata siswa
+let namas ='budi';
+let sekolah ='sdn negeri 1';
+let makana_favori='bakso';
+let alamat_tinggal='bantul';
+console.log('nama: '+namas,
+            'sekolah : '+sekolah,
+            ' makana favorit : '+makana_favori,
+            ' alamat : '+alamat_tinggal
+            );
---
+++ Javascript/day-2/script.js
@@ -0,0 +1,19 @@
+console.log('day-2');
+///// type data number
+let angka=34;
+let harga=35.000;
+let kecapata=4500;
+console.log('tipe data angka adalah :'+ typeof(angka));
+console.log('tipe data harga adalah :'+ typeof(harga));
+console.log('------tipe string-------');
+//tipe data string
+let biodata_nama='umar';
+let biodaya_hoby='main bola';
+let umur='123';
+console.log('tipe data biodata_nama adalah :'+ typeof(biodata_nama));
+console.log('tipe data umur adalah :'+ typeof(umur));
+console.log('------ tipe data function -----');
+function makan(){
+    console.log('gorengan');
+}
+console.log('tipe data makan adalah :'+typeof(makan));
