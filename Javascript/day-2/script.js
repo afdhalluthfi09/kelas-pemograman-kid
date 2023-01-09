af63ee7b47a7f9ca0e3cffc03c4d9bc5eb065a09
Afdhal Luthfi <39642502+afdhalluthfi09@users.noreply.github.com>
2023-01-09 01:44:49 +0000

    c

--- Javascript/day-2/script.js
+++ Javascript/day-2/script.js
@@ -9,7 +9,7 @@ console.log('------tipe string-------');
 //tipe data string
 let biodata_nama='umar';
 let biodaya_hoby='main bola';
-let umur='123';
+let umur='12 Thn';
 console.log('tipe data biodata_nama adalah :'+ typeof(biodata_nama));
 console.log('tipe data umur adalah :'+ typeof(umur));
 console.log('------ tipe data function -----');
@@ -17,3 +17,16 @@ function makan(){
     console.log('gorengan');
 }
 console.log('tipe data makan adalah :'+typeof(makan));
+console.log('------- perhitungan ---------');
+let nomor1=34;
+let nomor2 =30;
+let total =nomor1 + nomor2;
+let totalPengurangan=nomor1-nomor2;
+let totalPembagian=nomor1/nomor2;
+let totalPerkalian=nomor1*nomor2;
+(34 +30)x30-(40*45)/30
+let totalKompleks=(nomor1 + nomor2)*totalPembagian -(totalPembagian *totalPerkalian)/nomor2;
+console.log('jumlah dari perhitungan 34 + 30 = '+total);
+console.log('jumlah dari perhitungan 34 - 30 = '+totalPengurangan);
+console.log('jumlah dari perhitungan 34 / 30 = '+totalPembagian);
+console.log('jumlah dari perhitungan 34 * 30 = '+totalPerkalian);
