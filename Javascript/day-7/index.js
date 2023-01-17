6e2c9f1413fbd7b0ef4957882d03a2ea51977a36
Afdhal Luthfi <39642502+afdhalluthfi09@users.noreply.github.com>
2023-01-17 02:57:39 +0000

    day-8

--- Javascript/day-7/index.js
+++ Javascript/day-7/index.js
@@ -18,15 +18,29 @@ let dataSiswaNgajiC ={
     nama :"umar2",
     kelas:"juz 30",
     umur:9,
-    hobi:["mengaji","main sepeda"],
+    hobi:[],
     lulus:false,
 }
-/* kelas.push(dataSiswaNgajiA)
-kelas.push(dataSiswaNgajiB)
-kelas.push(dataSiswaNgajiC) */
-kelas.push(dataSiswaNgajiA,
-    dataSiswaNgajiB,
-    dataSiswaNgajiC)
-//cara nampilin
-// const umurSantri = kelas.filter(s => s.umur >10);
+
+console.log(dataSiswaNgajiA);
+console.log(dataSiswaNgajiB);
+console.log(dataSiswaNgajiC);
+kelas.push(dataSiswaNgajiA,dataSiswaNgajiB,dataSiswaNgajiC);
 console.log(kelas);
+//buid-in function  filter
+const filterUmur = kelas.filter((siswa)=>{ return siswa.umur < 10});
+console.log(filterUmur);
+
+//cek hobiy yang belum terisi
+const filterHobiKosong =kelas.filter((siswa) =>{return siswa.hobi == 0})
+console.log(filterHobiKosong);
+
+// mencari nama dalam datadiri
+const filterNamaSiswa = kelas.filter((siswa)=>{ return siswa.nama == "umar abdurrahman"})
+console.log(filterNamaSiswa);
+alertCekNama("umar abdurrahman");
+
+function alertCekNama(param){
+    const filterNamaSiswa = kelas.filter((siswa)=>{ return siswa.nama == param})
+    alert(` siswa dengan nama ${filterNamaSiswa[0].nama}, sudah terdaftar`);
+}
